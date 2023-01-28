50049b94f6a0f10ca489ec7ce018183c1d344160
Israel Aristide <44146685+IzzyDotExe@users.noreply.github.com>
2023-01-28 15:19:17 -0500

    fix transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,25 @@ setTimeout(() => {
     let transition = document.querySelector("#transition")
     transition.style.display = "none"
 }, 1500);
+
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains("navbutton")) {
+        e.preventDefault();
+        leave2(e.target.attributes.href.value);
+    }
+})
+
+function leave2(loc) {
+
+    rain = document.getElementById("transition-in"); 
+    rain.hidden = false;
+    rain.setAttribute("transition-style", "in:wipe:bottom-right");
+
+
+        setTimeout(() => {
+            location.href=loc;
+        }, 1500);
+
+
+}
